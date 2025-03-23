edb0f150abc7815e064cd76f5c1192b92a9b9ba3
londoncute <107755910+londoncute@users.noreply.github.com>
2025-03-23 21:49:41 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
 // Copyright 2022 UNN-IASR
-#include "fun.h"
+#include <gtest/gtest.h>
+#include "../include/fun.h"
 
-int main() {
-    return 0;
+// Тест для faStr1 (слова без цифр)
+TEST(FaStr1Test, BasicCases) {
+    EXPECT_EQ(faStr1("Hello world 123 abc"), 2); // "Hello", "world"
+    EXPECT_EQ(faStr1("123 456 789"), 0); // Нет слов без цифр
+    EXPECT_EQ(faStr1("one two three"), 3); // Все слова без цифр
+}
+
+// Тест для faStr2 (слова с заглавной буквы и только строчными буквами)
+TEST(FaStr2Test, BasicCases) {
+    EXPECT_EQ(faStr2("Hello world Apple banana"), 2); // "Hello", "Apple"
+    EXPECT_EQ(faStr2("Zebra Cat Dog"), 3); // Все слова подходят
+    EXPECT_EQ(faStr2("HELLO world aPPle"), 0); // Нет подходящих слов
+}
+
+// Тест для faStr3 (средняя длина слова)
+TEST(FaStr3Test, BasicCases) {
+    EXPECT_EQ(faStr3("Hello world this is test"), 4); // (5+5+4+2+4) / 5 = 4
+    EXPECT_EQ(faStr3("One two three four"), 4); // (3+3+5+4) / 4 = 4
+    EXPECT_EQ(faStr3("   "), 0); // Нет слов
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
